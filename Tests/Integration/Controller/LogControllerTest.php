27212649768b88cc0142a5e52916bbcf59f6f67d
Hannes Schulz <schulz@daten-bahn.de>
2016-05-02 22:53:26 +0200

    fixed path to logfile in unittest

--- Tests/Integration/Controller/LogControllerTest.php
+++ Tests/Integration/Controller/LogControllerTest.php
@@ -37,13 +37,7 @@ class LogControllerTest extends WebTestCase
         $this->assertEquals(204, $client->getResponse()->getStatusCode());
         $this->assertEmpty($client->getResponse()->getContent());
 
-        // in symfony version <= 2.7 we get an error if class name of definition is not set
-        if (version_compare(Kernel::VERSION, '2.8.0', '<')) {
-            $filename = static::$kernel->getLogDir().'/test.log';
-        }
-        else{
-            $filename = static::$kernel->getLogDir().'/foobar.log';
-        }
+        $filename = static::$kernel->getLogDir().'/test.log';
 
         $this->assertFileExists($filename);
         $this->assertContains('This is a log message', file_get_contents($filename));
